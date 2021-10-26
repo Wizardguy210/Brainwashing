3e04525ea2d5a95116eb5f169fddc114e1433471
Karshtakavaar <88218974+Karshtakavaar@users.noreply.github.com>
2021-10-26 16:53:30 -0500

    Create BadSteal.lua

---
+++ BadSteal.lua
@@ -0,0 +1,17 @@
+--// ROFL \\--
+
+local SwagDealer = game:GetService('Players').LocalPlayer
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Karshtakavaar/WallySV2XD/main/Waluh.lua", true))()
+local win1 = library:CreateWindow('Poopinator')
+local Core = game:GetService('CoreGui')
+Core.ScreenGui.Name = 'Wally'
+
+
+--// Ok \\ --
+win1:Button('Steal', function()
+for i,v in pairs(game:GetService('Workspace'):GetDescendants()) do
+    if v.Name == 'BoomBox' and v:IsA'Tool' then
+v.Handle.CFrame = SwagDealer.Character.HumanoidRootPart.CFrame
+end  
+end    
+end)
