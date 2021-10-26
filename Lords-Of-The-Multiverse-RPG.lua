5cdc9a4356c0c01fb22f786b6879f68da1d01574
Karshtakavaar <88218974+Karshtakavaar@users.noreply.github.com>
2021-10-26 15:52:15 -0500

    Added Private Universes, added more armors

--- Lords-Of-The-Multiverse-RPG.lua
+++ Lords-Of-The-Multiverse-RPG.lua
@@ -1,11 +1,12 @@
 local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Karshtakavaar/WallySV2XD/main/Waluh.lua', true))()
-local Window1 = library:CreateWindow('Poop')
+local Window1 = library:CreateWindow('Farm Stuff')
 local Core = game:GetService('CoreGui')
 local Level = game:GetService('Players').LocalPlayer.leaderstats.Lvl
 local Me = game:GetService('Players').LocalPlayer
 local Sword = nil
 local Char = Me.Character
-
+local window2 = library:CreateWindow('Armors')
+local window3 = library:CreateWindow('Private Universes')
 
 Core.ScreenGui.Name = 'Wally'
 Window1:Button('Show Level', function()
@@ -34,16 +35,32 @@ task.spawn(function()
 
 Window1:Toggle('Auto Swing', {flag = 'Autoswing'})
 
-Window1:Button('Teleport To op armor Universe 1', function()
+window2:Button('Godly Armor UNI 1', function()
     Char.HumanoidRootPart.CFrame = CFrame.new(741.391, 13.758, 464.647)
     
 end)
 
-Window1:Button('Teleport to op armor Universe 2', function()
-   Char.HumanoidRootPart.CFrame = CFrame.new(-604.495, 23.587, 794.749) 
+window2:Button('Godly Armor UNI 2', function()
+    Char.HumanoidRootPart.CFrame = CFrame.new(-604.495, 23.587, 794.749)  
+end)
+
+window2:Button('Goldy Armor UNI 3', function()
+    Char.HumanoidRootPart.CFrame = CFrame.new(-605.5, 19.859, 802.5)
+end)
+
+window3:Button('Instructions', function()
+messagebox('You have to be in universe 1 to use these', '', 0)    
+end)
+
+window3:Button('Universe 2', function()
+    Char.HumanoidRootPart.CFrame = CFrame.new(-140.499, 15.665, -931.973)  
     
 end)
 
+window3:Button('Universe 3', function()
+    Char.HumanoidRootPart.CFrame = CFrame.new(-110.5, 11.94, -1105.5)  
+    
+end)
 
 Window1:Button('Destroy', function()
     Core.Wally:Destroy()
