1390a7719890e83197dc51c0e22e45c79cd5631f
Karshtakavaar <88218974+Karshtakavaar@users.noreply.github.com>
2021-10-27 20:54:34 -0500

    Create AutoUnequipper.lua

---
+++ AutoUnequipper.lua
@@ -0,0 +1,12 @@
+local LP = game:GetService('Players').LocalPlayer
+_G.A = true
+    while _G.A do
+        wait()
+pcall(function()
+    for i,v in pairs(LP.Character:GetDescendants()) do
+    if v:IsA'Tool' then
+        v.Parent = LP.Backpack
+end
+end
+end)
+end
