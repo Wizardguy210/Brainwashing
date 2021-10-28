6e748a2a9819b8c23bebb9f484735f73c7fa6273
Karshtakavaar <88218974+Karshtakavaar@users.noreply.github.com>
2021-10-27 21:16:24 -0500

    Update AutoUnequipper.lua

--- AutoUnequipper.lua
+++ AutoUnequipper.lua
@@ -15,3 +15,22 @@ end
 end
 end)
 end
+
+
+--[[
+Alterior Anti kill ig
+local Humanoid = game:GetService('Players').LocalPlayer.Character.HumanoidRootPart
+_G.A = false
+while _G.A do
+    wait()
+ Humanoid.Anchored = true
+ wait(.2)
+ 
+ Humanoid.Anchored = false
+end
+
+
+
+
+
+--]]
