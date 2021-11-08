cd904117a0d4dc4cfa4ba2d4fdcf03e3bce73706
Karshtakavaar <88218974+Karshtakavaar@users.noreply.github.com>
2021-11-08 15:18:06 -0600

    Update BadSteal.lua

--- BadSteal.lua
+++ BadSteal.lua
@@ -1,6 +1,7 @@
 --// ROFL \\--
 
 local SwagDealer = game:GetService('Players').LocalPlayer
+local Humanoid = SwagDealer.Character.Humanoid
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Karshtakavaar/WallySV2XD/main/Waluh.lua", true))()
 local win1 = library:CreateWindow('Poopinator')
 local Core = game:GetService('CoreGui')
@@ -8,13 +9,18 @@ Core.ScreenGui.Name = 'Wally'
 
 
 --// Ok \\ --
-win1:Button('Steal', function()
-for i,v in pairs(game:GetService('Workspace'):GetDescendants()) do
-    if v:IsA'Tool' then
-v.Handle.CFrame = SwagDealer.Character.HumanoidRootPart.CFrame
-end  
-end    
-end)
+task.spawn(function()
+    while task.wait() do 
+        if win1.flags.Steal then
+    for i,v in pairs(game:GetService('Workspace'):GetChildren()) do
+        if v:IsA'Tool' then
+                        v.Handle.CFrame = SwagDealer.Character.HumanoidRootPart.CFrame
+                    end  
+                end  
+            end
+        end
+    end)
+win1:Toggle('Steal Tools', {flag = 'Steal'})
 
 win1:Button('Drop Tools', function()
   for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
