f13579578869b0ef4e47d99b6a2f0d80291103e5
Karshtakavaar <88218974+Karshtakavaar@users.noreply.github.com>
2021-11-23 14:06:34 -0600

    Update BadSteal.lua

--- BadSteal.lua
+++ BadSteal.lua
@@ -1,8 +1,11 @@
 --// ROFL \\--
 
-local SwagDealer = game:GetService('Players').LocalPlayer
-local Humanoid = SwagDealer.Character.Humanoid
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Karshtakavaar/WallySV2XD/main/Waluh.lua", true))()
+local me = game:GetService('Players').LocalPlayer
+local back = me.Backpack
+local char = me.Character
+local Humanoid = char.Humanoid
+local RootPart = char.HumanoidRootPart
+local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Karshtakavaar/WallySV2XD/main/Waluh.lua', true))()
 local win1 = library:CreateWindow('Poopinator')
 local Core = game:GetService('CoreGui')
 Core.ScreenGui.Name = 'Wally'
@@ -12,24 +15,23 @@ Core.ScreenGui.Name = 'Wally'
 task.spawn(function()
     while task.wait(.4) do 
         if win1.flags.Steal then
-    for i,v in pairs(game:GetService('Workspace'):GetChildren()) do
-        if v:IsA'Tool' then
-                        v.Handle.CFrame = SwagDealer.Character.HumanoidRootPart.CFrame
-                    end  
-                end  
+   for i,v in pairs(game:GetService('Workspace'):GetDescendants()) do
+    if v:IsA'Tool' and v.Name == 'Boombox' or v.Name == 'BoomBox' or v.Name == 'SuperFlyGoldenBoombox' then
+                     Humanoid:EquipTool(v)        
+                    end
+                end
             end
         end
     end)
 win1:Toggle('Steal Tools', {flag = 'Steal'})
 
 win1:Button('Drop Tools', function()
-  for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-    v.Parent = SwagDealer.Character
+  for i,v in pairs(back:GetChildren()) do
+    v.Parent = char
     v.Parent = workspace
 end              
 end)
 
 win1:Button('Destroy', function()
  Core.Wally:Destroy()       
-        
 end)
