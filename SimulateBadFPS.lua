806df75d78ed533ef9873172a3768d54dc585529
Harlan, T <88218974+Karshtakavaar@users.noreply.github.com>
2022-01-30 19:35:09 -0600

    Merged into Multi

--- SimulateBadFPS.lua
+++
@@ -1,9 +0,0 @@
-local Humanoid = game:GetService('Players').LocalPlayer.Character.HumanoidRootPart
-_G.A = true
-while _G.A do
-    wait()
- Humanoid.Anchored = true
- wait(.5)
- 
- Humanoid.Anchored = false
-end
