da6954db7704e2abaaa94d8d386246379ded4371
Harlan, T <88218974+Karshtakavaar@users.noreply.github.com>
2022-06-04 17:47:03 -0500

    Update and rename WIP.lua to GoalGame.lua

--- GoalGame.lua
+++ GoalGame.lua
@@ -1,5 +1,6 @@
 local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Karshtakavaar/Folder/main/WallysUI', true))()
 -- ^ TY WALLY!!! --
+local Player = game:GetService('Players').LocalPlayer
 local win1 = library:CreateWindow('Main')
 local win2 = library:CreateWindow('Teleports')
 local win3 = library:CreateWindow('Unboxing')
@@ -8,9 +9,15 @@ local Core = game:GetService('CoreGui')
 local remote = game:GetService('ReplicatedStorage').Remote.RemoteFunction
 local locations = game:GetService('Workspace').Ignore.HighlightSpawns
 local SkyIsland = game:GetService('Workspace').Ignore.BoostedShotArea.SkyIsland
-local HumanoidRootPart = game:GetService('Players').LocalPlayer.Character.HumanoidRootPart
 local chests = game:getService('Workspace').ChestStands
 Core.ScreenGui.Name = 'MAINUI'
+local Character = Player.Character
+local HumanoidRootPart = Character.HumanoidRootPart
+
+Player.CharacterAdded:Connect(function(NewCharacter)
+    Character = NewCharacter
+    HumanoidRootPart = NewCharacter:WaitForChild('HumanoidRootPart')
+end)
 
 
 task.spawn(function()
@@ -51,6 +58,10 @@ task.spawn(function()
 end)
 win1:Toggle('Upgrade Accuracy', {flag = 'Upgrade3'})
 
+win1:Button('Rebirth', function()
+   remote:InvokeServer('Rebirth') 
+end)
+
 
 win2:Dropdown('Teleports', {
     location = _G;
@@ -64,25 +75,34 @@ win2:Dropdown('Teleports', {
       'Rocket Ship';
       'Moon';
       'Moon Base';
+      'Mars';
+      'World 2 Portal';
+      'Mars Rocket Ship';
       'Sky Island';
     }
 }, function(Location)
  if Location == 'Spawn' then
-     HumanoidRootPart.CFrame = locations.Default.CFrame
+     HumanoidRootPart.CFrame = locations['Default'].CFrame
  elseif Location == 'Park' then
-     HumanoidRootPart.CFrame = locations.Park.CFrame
+     HumanoidRootPart.CFrame = locations['Park'].CFrame
  elseif Location == 'Desert' then
-     HumanoidRootPart.CFrame = locations.Desert.CFrame
+     HumanoidRootPart.CFrame = locations['Desert'].CFrame
  elseif Location == 'City' then
-     HumanoidRootPart.CFrame = locations.City.CFrame
+     HumanoidRootPart.CFrame = locations['City'].CFrame
  elseif Location == 'Beach' then
-     HumanoidRootPart.CFrame = locations.Beach.CFrame
+     HumanoidRootPart.CFrame = locations['Beach'].CFrame
  elseif Location == 'Rocket Ship' then
      HumanoidRootPart.CFrame = locations['Rocket Ship'].CFrame
  elseif Location == 'Moon' then
-     HumanoidRootPart.CFrame = locations.Moon.CFrame
+     HumanoidRootPart.CFrame = locations['Moon'].CFrame
  elseif Location == 'Moon Base' then
      HumanoidRootPart.CFrame = locations['Moon Base'].CFrame
+ elseif Location == 'Mars' then
+     HumanoidRootPart.CFrame = locations['Mars'].CFrame
+ elseif Location == 'World 2 Portal' then
+     HumanoidRootPart.CFrame = locations['World 2 Portal'].CFrame
+ elseif Location == 'Mars Rocket Ship' then
+     HumanoidRootPart.CFrame = locations['Mars Rocket Ship'].CFrame
  elseif Location == 'Sky Island' then
      HumanoidRootPart.CFrame = SkyIsland.CFrame
     end
@@ -100,7 +120,8 @@ win3:Dropdown('Chests', {
     'Space Chest';
     'Moon Chest';
     'Moon Base Chest';
-  
+    'World 2 Portal Chest';
+    'Floppa Chest';
     }
 }, function(Open)
     if Open == 'Sports Chest' then
@@ -119,10 +140,18 @@ win3:Dropdown('Chests', {
         remote:InvokeServer('PromptPurchaseChest', chests['Moon Chest'])
     elseif Open == 'Moon Base Chest' then
         remote:InvokeServer('PromptPurchaseChest', chests['Moon Base Chest'])
+    elseif Open == 'World 2 Portal Chest' then
+        remote:InvokeServer('PromptPurchaseChest', chests['World 2 Portal Chest'])
+    elseif Open == 'Floppa Chest' then
+        remote:InvokeServer('PromptPurchaseChest', chests['Floppa Chest'])
     end
 end)
 
 win4:Button('Destroy', function()
    game:GetService('CoreGui').MAINUI:Destroy() 
     
+end)
+
+win4:Button('Beast Mode', function()
+Player:Kick('Nigger')
 end)
