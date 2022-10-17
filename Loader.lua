7218b0f06d7f65cf26844e041ee6d6be1394e3ff
Harlan, T <88218974+Karshtakavaar@users.noreply.github.com>
2022-10-17 14:08:20 -0500

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,8 +0,0 @@
--- Loader
-if game.GameId == 3475191052 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Karshtakavaar/Brainwashing/main/GoalGame.lua', true))()-- Goal
-    elseif game.GameId == 2998444255 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Karshtakavaar/Brainwashing/main/SlashingSimulator.lua', true))()-- Goal
-    elseif game.GameId ~= 3475191052 or 2998444255 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Karshtakavaar/Brainwashing/main/Multi.lua', true))()-- Goal
-end
