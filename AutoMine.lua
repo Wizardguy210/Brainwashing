6b4e9bba1fd5630719207c23c9990045ce2a066d
Harlan, T <88218974+Karshtakavaar@users.noreply.github.com>
2022-10-17 22:18:26 -0500

    Added auto equip, Teleport region fixer

--- AutoMine.lua
+++ AutoMine.lua
@@ -1,9 +1,11 @@
 local Player = game:GetService('Players')['LocalPlayer']
 local Character = Player['Character']
 local RootPart = Character['HumanoidRootPart']
+local Humanoid = Character['Humanoid']
 Player.CharacterAdded:Connect(function(c)
    Character = c
    RootPart = c:WaitForChild('HumanoidRootPart')
+   Humanoid = c:WaitForChild('Humanoid')
 end)
 
 
@@ -14,12 +16,26 @@ CoreGui.ScreenGui.Name = 'MainUI'
 
 local AdvancedMine = game:GetService('Workspace')['Global']['OreMines2']
 local Mine = game:GetService('Workspace')['Global']['OreMines']
+local Teleport = game:GetService('ReplicatedStorage').Remotes.Teleport
+
+
+
+local Equip = function()
+    for _, v in next, Player['Backpack']:GetChildren() do
+        if string.find(tostring(v), 'Pickaxe') then
+            Humanoid:EquipTool(v)
+        end
+    end
+end
+
 
 
 
 task.spawn(function()
     while task.wait() do
-        if Auto.flags.Mine2 then
+        if Auto.flags.Mine1 then
+            Equip()
+            Teleport:InvokeServer(Character, Vector3.new(0, -1013, 0))
             for _, v in next, Mine:GetDescendants() do
                 if string.find(tostring(v), 'Head') then
                     if v.Parent.Name == 'Ruby ore' then
@@ -32,13 +48,16 @@ task.spawn(function()
                 end
             end
         end)
-    Auto:Toggle('Mine Normal', {flag = 'Mine2'})
+    Auto:Toggle('Mine Normal', {flag = 'Mine1'})
 
 task.spawn(function()
     while task.wait() do
-        if Auto.flags.Mine1 then
+        if Auto.flags.Mine2 then
+            
+      Equip()
+            Teleport:InvokeServer(Character, Vector3.new(0, -1085, 0))
             for _, v in next, AdvancedMine:GetDescendants() do
-                if string.find(tostring(v), 'Head') then
+                    if string.find(tostring(v), 'Head') then
                         if v.Parent.Name == 'Ruby ore' then
                                 v:Destroy()
                                 else
@@ -49,10 +68,9 @@ task.spawn(function()
                     end
                 end
             end)
-    Auto:Toggle('Mine Advanced', {flag = 'Mine1'})
+    Auto:Toggle('Mine Advanced', {flag = 'Mine2'})
 
 
 Auto:Button('Destroy', function()
    CoreGui['MainUI']:Destroy()
 end)
-
